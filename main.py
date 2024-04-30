1e25013286d7c3da098c290a275c7a32066baad4
Nudlik <gegorp@mail.ru>
2024-05-01 03:38:03 +0700

    обрезка картинки

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+import os.path
+
 from utils.screenshot import ScreenShot
+from PIL import Image
 
 PARS_DATA = [
     {
         'url': 'https://stepik.org/users/564742454/',
         'file_name': 'stepik_stats.png',
-        'tag': '.cal-heatmap-container'
+        'tag': '.cal-heatmap-container',
+        'coords': (398, 189, 1152, 451)
     },
     {
         'url': 'https://codeium.com/profile/nudlik',
         'file_name': 'codeium_stats.png',
-        'tag': 'body'
+        'tag': 'body',
+        'coords': (457, 434, 1164, 560)
     }
 ]
 
@@ -21,10 +26,17 @@ def download_screenshots(browser_name, folder_img, data):
 
 
 def refactor_screenshots(folder_img, data):
-    pass
+    for item in data:
+        path = os.path.join(folder_img, item['file_name'])
+        img = Image.open(path)
+        cropped_img = img.crop(item['coords'])
+        cropped_img.save(os.path.join(folder_img, 'result_' + item['file_name']))
+        img.close()
+        cropped_img.close()
 
 
 if __name__ == '__main__':
     web_browser_name = 'firefox'
     root_img_dir = 'img'
     download_screenshots(web_browser_name, root_img_dir, PARS_DATA)
+    refactor_screenshots(root_img_dir, PARS_DATA)
