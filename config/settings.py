5f5b6e17d33ca7e220f3c117b45bd3efa9525bed
Nudlik <gegorp@mail.ru>
2024-05-05 01:41:46 +0700

    подобрал разрешение, молимся что бы при следующих экшенах сервак оставался с постоянным монитором D:

--- config/settings.py
+++ config/settings.py
@@ -4,15 +4,14 @@ PARS_DATA = [
         'url': 'https://stepik.org/users/564742454/',
         'file_name': 'stepik_stats.png',
         'tag': '.cal-heatmap-container',
-        'coords_local': (398, 189, 1152, 451),
+        'coords_local': (391, 189, 1146, 451),
         'coords_remote': (398, 189, 1152, 451)
     },
     {
         'url': 'https://codeium.com/profile/nudlik',
         'file_name': 'codeium_stats.png',
         'tag': 'body',
-        'coords_local': (457, 426, 1164, 560),
-        # 'coords_remote': (304, 435, 955, 558),
+        'coords_local': (430, 430, 1101, 560),
         'coords_remote': (457, 426, 1164, 560),
     }
 ]
